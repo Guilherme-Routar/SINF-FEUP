d943adaf36a256efa53a17242fe7014ae1405dd4
LuísCosta <luistelmocosta@gmail.com>
2016-10-12 23:43:41 +0100

    Added devise

--- sinf-webstore/config/environments/development.rb
+++ sinf-webstore/config/environments/development.rb
@@ -26,4 +26,6 @@ SinfWebstore::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.default_ur_options = {host: 'localhost', port: 3000}
 end
