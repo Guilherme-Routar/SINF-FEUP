afbffa7d211716f6b297673e80a30decd21d6014
luistelmocosta <luistelmocosta@gmail.com>
2016-11-22 00:33:47 +0000

    Added images and resources

--- sinf-webstore/app/models/book.rb
+++ sinf-webstore/app/models/book.rb
@@ -1,8 +1,8 @@
 class Book < ActiveRecord::Base
 
-	belongs_to :user
-	has_attached_file :image
-	has_attached_file :resource
+	extend FriendlyId
+	friendly_id :name, use: :slugged
+
 
 	def self.search(search)
 		if search
@@ -11,4 +11,20 @@ class Book < ActiveRecord::Base
 			all
 		end
 	end
+
+	belongs_to :user
+	has_attached_file :image 
+	has_attached_file :resource
+
+	validates_attachment_content_type :image, 
+	content_type:  /^image\/(png|gif|jpeg)/,
+	message: "Only images allowed"
+	
+	validates_attachment_content_type :resource, 
+	content_type: ['application/pdf'],
+	message: "Only pdfs allowed"	
+
+	validates :image, attachment_presence: true 
+	validates :resource, attachment_presence: true 
+
 end
--- sinf-webstore/db/schema.rb
+++ sinf-webstore/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121222539) do
+ActiveRecord::Schema.define(version: 20161122000649) do
 
   create_table "books", force: true do |t|
     t.string   "name"
@@ -22,7 +22,31 @@ ActiveRecord::Schema.define(version: 20161121222539) do
     t.integer  "user_id"
     t.boolean  "availability"
     t.integer  "price"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.string   "resource_file_name"
+    t.string   "resource_content_type"
+    t.integer  "resource_file_size"
+    t.datetime "resource_updated_at"
+    t.string   "slug"
   end
+
+  add_index "books", ["slug"], name: "index_books_on_slug", unique: true
+
+  create_table "friendly_id_slugs", force: true do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
