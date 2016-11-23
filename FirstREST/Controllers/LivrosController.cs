be9e31e4153065c47fa23cfab4796bc2b32445a4
AndreMachado94 <andremachado94@gmail.com>
2016-11-23 20:56:43 +0000

    Book Query working - Top Books not working

---
+++ FirstREST/Controllers/LivrosController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FirstREST.Lib_Primavera.Model;
+
+namespace FirstREST.Controllers
+{
+    public class LivrosController : ApiController
+    {
+        //
+        // GET: /Artigos/
+
+        public IEnumerable<Lib_Primavera.Model.Artigo> Get()
+        {
+            return Lib_Primavera.PriIntegration.ListaLivros();
+        }
+
+
+        // GET api/artigo/5    
+        public Artigo Get(string id)
+        {
+            Lib_Primavera.Model.Artigo artigo = Lib_Primavera.PriIntegration.GetArtigo(id);
+            if (artigo == null)
+            {
+                throw new HttpResponseException(
+                  Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+            else
+            {
+                return artigo;
+            }
+        }
+
+    }
+}
