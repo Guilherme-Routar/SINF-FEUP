eb10d3c45344b8e172311007fe185c7f4e84b2e0
Luis Costa <luistelmocosta@gmail.com>
2016-11-24 20:02:40 +0000

    Integration done

---
+++ webstore/app/assets/javascripts/application.js
@@ -0,0 +1,35 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+<<<<<<< HEAD
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+=======
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+>>>>>>> 52be9f2e6e93b0af70775277c5823de5e5edbd3a
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+<<<<<<< HEAD
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//tirei o jquery turbolinks
+//= require jquery_ujs
+//tirei o turbolinks
+//= require bootstrap-sprockets
+//= require ./utilities
+//= require_tree ./common
+
+=======
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require twitter/bootstrap
+//= require turbolinks
+//= require_tree .
+>>>>>>> 52be9f2e6e93b0af70775277c5823de5e5edbd3a
